d774741fd6c4ce5ea8291a94b7b37cdaffdb67fe
Taranjitkaur02 <tanukaur2003december@gmail.com>
2025-07-05 14:35:21 +0530

    Create App.jsx

---
+++ react_practice/src/App.jsx
@@ -0,0 +1,15 @@
+import Intro from "./component/Intro"
+
+
+function App() {
+  
+  return (
+   <>
+    <Intro></Intro>
+    <Intro></Intro>
+    <Intro></Intro>
+   </>
+  )
+}
+
+export default App
